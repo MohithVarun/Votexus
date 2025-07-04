5738395b502f9b1ac6e124fb7b36d7ede0c1dde4
Mohith Varun Regalagadda <157415164+MohithVarun@users.noreply.github.com>
2025-07-04 18:24:34 +0530

    Add files via upload

---
+++ client/src/pages/Logout.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import {voteActions} from '../store/vote-slice'
+
+const Logout = () => {
+  const dispatch=useDispatch()
+  const navigate = useNavigate()
+  
+  useEffect(()=>{
+    dispatch(voteActions.changeCurrentVoter(null))
+    localStorage.removeItem("currentUser")
+    navigate('/')
+  },[])
+
+  return (
+    <></>
+  )
+}
+
+export default Logout
